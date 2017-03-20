644cec5aeff5e5c1f94eca3ac6ebfa07ed420542
Hang Yan <hangyan@alauda.io>
2017-03-20 18:08:40 +0800

    Start script

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
 
 pip install jedi
 # M-x jedi:install-server
+
+
+# locate
+test -f /var/db/locate.database || sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
