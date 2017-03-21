af686f9d1437d285d16c708caf25094a5be0a751
Hang Yan <hangyan@alauda.io>
2017-03-21 17:36:41 +0800

    Kill buffers function

--- lisp/init-func.el
+++ lisp/init-func.el
@@ -1,8 +1,16 @@
-(defun only-current-buffer ()
-  (interactive)
-  (mapc 'kill-buffer (cdr (buffer-list (current-buffer)))))
+(defun kill-some-buffer (buffer)
+  "Ignore some buffers."
+  (if (member (buffer-name buffer) '("*Messages*" "*anaconda-mode*"))
+      (message "Ignore you : %s" (buffer-name Buffer))
+    (kill-buffer buffer)))
 
+
+(defun only-current-buffer ()
+  "Kill other buffers."
+  (interactive)
+  (mapc 'kill-some-buffer (cdr (buffer-list (current-buffer)))))
 
 
 
 (provide 'init-func)
+;;; init-func.el ends here
