18d23ebad7b3247c01d1b8b3380c673dfecdce08
Hang Yan <hangyan@alauda.io>
2017-03-22 22:27:56 +0800

    Update

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -316,4 +316,25 @@ With arg N, insert N newlines."
             (diminish 'guide-key-mode)))
 
 
+(defun move-line-down ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion
+      (forward-line)
+      (transpose-lines 1))
+    (forward-line)
+    (move-to-column col)))
+
+(defun move-line-up ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion
+      (forward-line)
+      (transpose-lines -1))
+    (move-to-column col)))
+
+(global-set-key (kbd "<C-s-down>") 'move-line-down)
+(global-set-key (kbd "<C-s-up>") 'move-line-up)
+
+
 (provide 'init-editing-utils)
--- lisp/init-git.el
+++ lisp/init-git.el
@@ -76,4 +76,17 @@
 (global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
 
 
+
+;; C-c C-a to amend without any prompt
+
+(defun magit-just-amend ()
+  (interactive)
+  (save-window-excursion
+    (magit-with-refresh
+     (shell-command "git --no-pager commit --amend --reuse-message=HEAD"))))
+
+(eval-after-load "magit"
+  '(define-key magit-status-mode-map (kbd "C-c C-a") 'magit-just-amend))
+
+
 (provide 'init-git)
