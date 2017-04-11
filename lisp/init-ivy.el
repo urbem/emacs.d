624e23ec9485da21f01092eadd5784c3a1aed219
Hang Yan <hangyan@alauda.io>
2017-04-11 10:25:42 +0800

    Update

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -1,25 +1,19 @@
 (when (maybe-require-package 'ivy)
-  (after-load 'ivy
-    (setq-default ivy-use-virtual-buffers t
-                  ivy-count-format ""
-                  projectile-completion-system 'ivy
-                  ivy-initial-inputs-alist
-                  '((counsel-M-x . "^")
-                    (man . "^")
-                    (woman . "^")))
-    ;; IDO-style directory navigation
-    (define-key ivy-minibuffer-map (kbd "C-j") #'ivy-immediate-done)
-    (define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done)
-    (when (maybe-require-package 'diminish)
-      (diminish 'ivy-mode)))
-
+  (after-load 'ivy (setq-default ivy-use-virtual-buffers t ivy-count-format ""
+                                 projectile-completion-system 'ivy ivy-initial-inputs-alist
+                                 '((counsel-M-x . "^")
+                                   (man . "^")
+                                   (woman . "^")))
+              ;; IDO-style directory navigation
+              (define-key ivy-minibuffer-map (kbd "C-j") #'ivy-immediate-done)
+              (define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done)
+              (when (maybe-require-package 'diminish)
+                (diminish 'ivy-mode)))
   (defun sanityinc/enable-ivy-flx-matching ()
     "Make `ivy' matching work more like IDO."
     (interactive)
     (require-package 'flx)
-    (setq-default ivy-re-builders-alist
-                  '((t . ivy--regex-fuzzy))))
-
+    (setq-default ivy-re-builders-alist '((t . ivy--regex-fuzzy))))
   (add-hook 'after-init-hook
             (lambda ()
               (when (bound-and-true-p ido-ubiquitous-mode)
@@ -32,8 +26,7 @@
 (when (maybe-require-package 'counsel)
   (setq-default counsel-mode-override-describe-bindings t)
   (when (maybe-require-package 'diminish)
-    (after-load 'counsel
-      (diminish 'counsel-mode)))
+    (after-load 'counsel (diminish 'counsel-mode)))
   (add-hook 'after-init-hook 'counsel-mode))
 
 
@@ -41,6 +34,6 @@
 ;;  (after-load 'ivy
 ;;    (define-key ivy-mode-map (kbd "C-s") 'swiper)))
 
-
+(setq ivy-ignore-buffers '("\\*anaconda-mode\\*"))
 
 (provide 'init-ivy)
