e08556318b6b79f750150bb572e9576d65884ea2
Hang Yan <hangyan@alauda.io>
2017-05-05 15:52:14 +0800

    Update

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -31,5 +31,13 @@
 
 (add-to-list 'company-backends 'company-c-headers)
 
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+
+
 (provide 'init-cc)
 ;;; init-cc.el ends here
