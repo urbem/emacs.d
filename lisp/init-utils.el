0397f1276140c6475ea450b59414d23267b997b0
Hang Yan <hangyan@alauda.io>
2019-03-28 00:55:07 +0800

    Update

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -117,4 +117,14 @@
 
 
 
+;; helpful
+;; Note that the built-in `describe-function' includes both functions
+;; and macros. `helpful-function' is functions only, so we provide
+;; `helpful-callable' as a drop-in replacement.
+(global-set-key (kbd "C-h f") #'helpful-callable)
+
+(global-set-key (kbd "C-h v") #'helpful-variable)
+(global-set-key (kbd "C-h k") #'helpful-key)
+
+
 (provide 'init-utils)
