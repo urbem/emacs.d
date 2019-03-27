16e8adcaae5f06c3448feb87d29b264c326034a0
Hang Yan <hangyan@alauda.io>
2019-03-28 00:27:53 +0800

    Update

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -134,7 +134,29 @@
 (require-package 'volatile-highlights)
 (volatile-highlights-mode t)
 
+(defun neotree-project-dir-toggle ()
+  "Open NeoTree using the project root, using find-file-in-project,
+or the current buffer directory."
+  (interactive)
+  (let ((project-dir (ignore-errors
+;;; Pick one: projectile or find-file-in-project
+                                        ; (projectile-project-root)
+                       (ffip-project-root)))
+        (file-name (buffer-file-name))
+        (neo-smart-open t))
+    (if (and (fboundp 'neo-global--window-exists-p)
+             (neo-global--window-exists-p))
+        (neotree-hide)
+      (progn (neotree-show)
+             (if project-dir (neotree-dir project-dir))
+             (if file-name (neotree-find file-name))))))
 
+
+;; neotree
+;; ---> package-install all-the-icons
+;; ---> all-the-icons-install-fonts
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(global-set-key [f5] 'neotree-project-dir-toggle)
 
 
 
