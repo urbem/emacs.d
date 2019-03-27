e83cfbabd85aa8ef2f7e3f78da67a52e85bfc942
Hang Yan <hangyan@alauda.io>
2019-03-27 23:34:19 +0800

    Update

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,8 +1,7 @@
 ;;; Find and load the correct package.el
 
 ;; When switching between Emacs 23 and 24, we always use the bundled package.el in Emacs 24
-(let ((package-el-site-lisp-dir
-       (expand-file-name "site-lisp/package" user-emacs-directory)))
+(let ((package-el-site-lisp-dir (expand-file-name "site-lisp/package" user-emacs-directory)))
   (when (and (file-directory-p package-el-site-lisp-dir)
              (> emacs-major-version 23))
     (message "Removing local package.el from load-path to avoid shadowing bundled version")
@@ -11,15 +10,15 @@
 (require 'package)
 
 ;; use china mirror
-(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                                                  ("melpa" . "http://elpa.emacs-china.org/melpa/")
-                                                                           ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")))
 
 
 
-(defconst sanityinc/no-ssl (or (< emacs-major-version 24)
-                               (and (memq system-type '(windows-nt ms-dos))
-                                    (not (gnutls-available-p)))))
+
+
+(defconst sanityinc/no-ssl
+  (or (< emacs-major-version 24)
+      (and (memq system-type '(windows-nt ms-dos))
+           (not (gnutls-available-p)))))
 
 ;;; On-demand installation of packages
 
@@ -27,16 +26,15 @@
   "Install given PACKAGE, optionally requiring MIN-VERSION.
 If NO-REFRESH is non-nil, the available package lists will not be
 re-downloaded in order to locate PACKAGE."
-  (if (package-installed-p package min-version)
-      t
-    (if (or (assoc package package-archive-contents) no-refresh)
-        (if (boundp 'package-selected-packages)
-            ;; Record this as a package the user installed explicitly
-            (package-install package nil)
-          (package-install package))
-      (progn
-        (package-refresh-contents)
-        (require-package package min-version t)))))
+  (if (package-installed-p package min-version) t (if (or (assoc package package-archive-contents)
+                                                          no-refresh)
+                                                      (if (boundp 'package-selected-packages)
+                                                          ;; Record this as a package the user installed explicitly
+                                                          (package-install package nil)
+                                                        (package-install package))
+                                                    (progn (package-refresh-contents)
+                                                           (require-package package min-version
+                                                                            t)))))
 
 
 (defun maybe-require-package (package &optional min-version no-refresh)
@@ -45,8 +43,7 @@ In the event of failure, return nil and print a warning message.
 Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
 available package lists will not be re-downloaded in order to
 locate PACKAGE."
-  (condition-case err
-      (require-package package min-version no-refresh)
+  (condition-case err (require-package package min-version no-refresh)
     (error
      (message "Couldn't install optional package `%s': %S" package err)
      nil)))
@@ -69,9 +66,15 @@ locate PACKAGE."
 (defun sanityinc/set-tabulated-list-column-width (col-name width)
   "Set any column with name COL-NAME to the given WIDTH."
   (when (> width (length col-name))
-    (cl-loop for column across tabulated-list-format
-             when (string= col-name (car column))
-             do (setf (elt column 1) width))))
+    (cl-loop
+     for
+     column
+     across
+     tabulated-list-format
+     when
+     (string= col-name (car column))
+     do
+     (setf (elt column 1) width))))
 
 (defun sanityinc/maybe-widen-package-menu-columns ()
   "Widen some columns of the package menu table to avoid truncation."
